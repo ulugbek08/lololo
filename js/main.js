edf6742a81aee1372dc46c1469818302ed5c8b1b
ulugbek08 <ulugbeknasyrov08@gmail.com>
2018-07-30 10:27:23 +0600

    1st

--- js/main.js
+++ js/main.js
@@ -35,19 +35,6 @@ $(document).ready(function(){
   });
 });
 
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    setTimeout(carousel, 1000);    
-}
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
